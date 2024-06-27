8d50822c263531d5ffba7efffb08068c9af7d136
Maan Ali <maan5315680@gmail.com>
2024-06-28 01:07:53 +0500

    initial commmit

---
+++ api/index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3000;
+
+if (!MONGO_URI) {
+  console.error("Missing MONGO_URI in environment variables");
+  process.exit(1); // Exit the application if the URI is not set
+}
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB: ", err);
+  });
+
+const __dirname = path.resolve();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
